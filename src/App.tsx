f15167c85009de4faa09ad707ab093fc319a3ca5
matthewjljackson <matthewjljackson@gmail.com>
2022-06-06 21:59:10 +0100

    add json-server package

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Solution } from './interfaces';
 
 function App() {
+  const [solution, setSolution] = useState<Solution | null>(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then((res) => res.json())
+      .then((json: Solution[]) => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Wordle (Lingo)</h1>
+      {solution && <div>{solution.word}</div>}
     </div>
   );
 }
